582514089db91f511f8faa14a51f0cbffce56d53
Cu Janeway <cu.janeway@gmail.com>
2021-11-02 22:35:47 +0000

    Implement zoom animation
    - Zoom animation implementation using scale and transform origin.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -40,17 +40,32 @@ const targetBehaviour = '_blank'
 // 'archive-circle-link','archive-link-1','archive-link-2']
 
 const linkIds = {
-        'google-link': googleLink,'microsoft365-link': microsoft365Link,'inner-circle-text-link':innerCircleTextLink,'joiners-link1': joinersLink1,'joiners-link2': joinersLink2,
-        'joiners-link3': joinersLink3,'movers-link1': moversLink1,'automate-link1': automateLink1,'migrate-circle-link': migrateCircleLink,'migrate-link1': migrateLink1,
-        'migrate-link2': migrateLink2,'migrate-link-3': migrateLink3,'starters-circle-link': startersCircleLink,'starters-link1': startersLink1,'starters-link2': startersLink2,
-        'starters-link3': startersLink3,'leavers-circle-link': leaversCircleLink,'leavers-link1': leaversLink1,'leavers-link2': leaversLink2,'archive-circle-link': archiveCircleLink,
-        'archive-link1': archiveLink1,'archive-link2': archiveLink2
-    }
+    'google-link': googleLink,'microsoft365-link': microsoft365Link,'inner-circle-text-link':innerCircleTextLink,'joiners-link1': joinersLink1,'joiners-link2': joinersLink2,
+    'joiners-link3': joinersLink3,'movers-link1': moversLink1,'automate-link1': automateLink1,'migrate-circle-link': migrateCircleLink,'migrate-link1': migrateLink1,
+    'migrate-link2': migrateLink2,'migrate-link-3': migrateLink3,'starters-circle-link': startersCircleLink,'starters-link1': startersLink1,'starters-link2': startersLink2,
+    'starters-link3': startersLink3,'leavers-circle-link': leaversCircleLink,'leavers-link1': leaversLink1,'leavers-link2': leaversLink2,'archive-circle-link': archiveCircleLink,
+    'archive-link1': archiveLink1,'archive-link2': archiveLink2
+}
+
+// Implement object that consists of key-val pairs for different zoom origins.
 
 window.onload = (event) => {
     // Load in the links once all resources on page have loaded.
-    for (var key in linkIds) {
-        document.getElementById(key).setAttribute('href', linkIds[key])
-        document.getElementById(key).setAttribute('target', targetBehaviour)
+    // for (var key in linkIds) {
+    //     document.getElementById(key).setAttribute('href', linkIds[key])
+    //     document.getElementById(key).setAttribute('target', targetBehaviour)
+    // }
+    
+    var zoomElements = document.getElementsByClassName('zoom-anchor')
+    
+    for (var i = 0; i < zoomElements.length; i++) {
+        zoomElements[i].addEventListener('click', function (e) {
+            e.preventDefault()
+            var svgContainer = document.getElementById('animation-widget')
+            var zoomOrigin = this.getAttribute('data-zoom-origin')
+
+            svgContainer.classList.add(zoomOrigin)
+            svgContainer.classList.add('zoom')
+        })
     }
 }
